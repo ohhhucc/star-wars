0923230ba3b82c536b2867e6e5c999902ed1a870
ohhhucc <76487504+ohhhucc@users.noreply.github.com>
2021-10-06 15:37:27 +0300

    add table and pagination to app file

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,6 @@
 import {useSelector} from "react-redux";
 import {selectPeople} from "../redux/reducers/people/selectors";
+import Table from "../components/Table";
 
 function App() {
 
@@ -15,32 +16,7 @@ function App() {
                     Loading...
                 </div>
             ) : (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>NAME</th>
-                            <th>BIRTH YEAR</th>
-                            <th>EYE COLOR</th>
-                            <th>GENDER</th>
-                            <th>HAIR COLOR</th>
-                            <th>HEIGHT</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {people?.data?.results.map(p => {
-                            return (
-                                <tr key={p.name}>
-                                    <td>{p.name}</td>
-                                    <td>{p.birth_year}</td>
-                                    <td>{p.eye_color}</td>
-                                    <td>{p.gender}</td>
-                                    <td>{p.hair_color}</td>
-                                    <td>{p.height}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
+                <Table people={people}/>
             )}
         </div>
     );

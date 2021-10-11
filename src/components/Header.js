5adbefcec854c8720cbc4c08714e2d390a703408
ohhhucc <76487504+ohhhucc@users.noreply.github.com>
2021-10-11 17:50:50 +0300

    add header.js file

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import styles from '../styles.module.css';
+
+export default function Header() {
+    return (
+        <div className={styles.header}>
+            <h1>STAR-WARS UI</h1>
+        </div>
+    )
+}
--- src/components/Table.js
+++ src/components/Table.js
@@ -2,8 +2,9 @@ import TablePagination from "./TablePagination";
 import {useDispatch, useSelector} from "react-redux";
 import {LOAD_PEOPLE} from "../redux/reducers/people/actions";
 import {selectPeople} from "../redux/reducers/people/selectors";
+import {Link} from "react-router-dom";
 
-export default function (props) {
+export default function () {
 
     const people = useSelector(selectPeople);
 
@@ -17,39 +18,70 @@ export default function (props) {
         }
     })
 
+    const search = (event) => dispatch({
+        type: LOAD_PEOPLE,
+        payload: {
+            page: 1,
+            search: event.target.value
+        }
+    })
+
     return (
         <>
-            <table>
-                <thead>
-                <tr>
-                    <th>NAME</th>
-                    <th>BIRTH YEAR</th>
-                    <th>EYE COLOR</th>
-                    <th>GENDER</th>
-                    <th>HAIR COLOR</th>
-                    <th>HEIGHT</th>
-                </tr>
-                </thead>
-                <tbody>
-                {people?.data?.results.map(p => {
-                    return (
-                        <tr key={p.name}>
-                            <td>{p.name}</td>
-                            <td>{p.birth_year}</td>
-                            <td>{p.eye_color}</td>
-                            <td>{p.gender}</td>
-                            <td>{p.hair_color}</td>
-                            <td>{p.height}</td>
+
+            <form>
+                <input
+                    type={'text'}
+                    placeholder={'SEARCH'}
+                    value={people.search}
+                    onChange={search}
+                />
+            </form>
+
+            {people.loading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>NAME</th>
+                            <th>BIRTH YEAR</th>
+                            <th>EYE COLOR</th>
+                            <th>GENDER</th>
+                            <th>HAIR COLOR</th>
+                            <th>HEIGHT</th>
+                            <th></th>
                         </tr>
-                    )
-                })}
-                </tbody>
-            </table>
-            <TablePagination
-                page={people.page}
-                total={people.data.count}
-                onChange={changePage}
-            />
+                        </thead>
+                        <tbody>
+                        {people?.data?.results.map(p => {
+
+                            const id = p.url.replaceAll(/\D/g, '');
+
+                            return (
+                                <tr key={p.name}>
+                                    <td>{p.name}</td>
+                                    <td>{p.birth_year}</td>
+                                    <td>{p.eye_color}</td>
+                                    <td>{p.gender}</td>
+                                    <td>{p.hair_color}</td>
+                                    <td>{p.height}</td>
+                                    <td>
+                                        <Link to={`/people/${id}`}>Details</Link>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                        </tbody>
+                    </table>
+                    <TablePagination
+                        page={people.page}
+                        total={people.data.count}
+                        onChange={changePage}
+                    />
+                </>
+            )}
         </>
     )
 }

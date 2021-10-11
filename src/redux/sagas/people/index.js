97f16eec0b5089e05cfccdd4dc53ee258b188c43
ohhhucc <76487504+ohhhucc@users.noreply.github.com>
2021-10-11 18:04:34 +0300

    add saga for details page for fetching a specific character

--- src/redux/sagas/people/index.js
+++ src/redux/sagas/people/index.js
@@ -1,26 +1,54 @@
 import {call, apply, put, takeLatest, take, select, fork} from "redux-saga/effects";
-import {LOAD_PEOPLE, LOAD_PEOPLE_SUCCESS} from "../../reducers/people/actions";
+import {matchPath} from "react-router";
+import {LOAD_PEOPLE, LOAD_PEOPLE_FAILURE, LOAD_PEOPLE_SUCCESS} from "../../reducers/people/actions";
+import {LOAD_DETAILS, LOAD_DETAILS_FAILURE, LOAD_DETAILS_SUCCESS} from "../../reducers/details/actions";
 import {LOCATION_CHANGE} from "connected-react-router";
 import {selectPeople} from "../../reducers/people/selectors";
+import {DETAILS_ROUTE, getConfig, APP_ROUTE} from "../../../routes";
 
-export function* fetchPeopleDetails() {
+
+export function* fetchDetails({payload}) {
+    try {
+        const {id} = payload;
+        const request = yield call(fetch, `https://swapi.dev/api/people/${id}`)
+        const data = yield apply(request, request.json);
+        yield put({
+            type: LOAD_DETAILS_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        yield put({
+            type: LOAD_DETAILS_FAILURE,
+            payload: error
+        })
+    }
 
 }
 
-export function* fetchPeopleList({payload}) {
-    const {page, search} = payload;
-    const request = yield call(fetch, `https://swapi.dev/api/people?page=${page}&search=${search}`)
-    const data = yield apply(request, request.json);
-    yield put({
-        type: LOAD_PEOPLE_SUCCESS,
-        payload: data
-    })
+export function* fetchPeople({payload}) {
+    try {
+        const {page, search} = payload;
+        const request = yield call(fetch, `https://swapi.dev/api/people?page=${page}&search=${search}`)
+        const data = yield apply(request, request.json);
+        yield put({
+            type: LOAD_PEOPLE_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        yield put({
+            type: LOAD_PEOPLE_FAILURE,
+            payload: error
+        })
+    }
+
 }
 
-export function* fetchPeopleOnRoute() {
+export function* fetchOnRoute() {
     while (true) {
         const action = yield take(LOCATION_CHANGE);
-        if (action.payload.location.pathname === '/') {
+        const mainPage = matchPath(action.payload.location.pathname, getConfig(APP_ROUTE));
+        const detailsPage = matchPath(action.payload.location.pathname, getConfig(DETAILS_ROUTE));
+        if (mainPage) {
             const state = yield select(selectPeople);
             const {page, search} = state;
             yield put({
@@ -30,10 +58,23 @@ export function* fetchPeopleOnRoute() {
                 }
             })
         }
+        if (detailsPage) {
+            const {id} = detailsPage.params;
+
+            if(id) {
+                yield put({
+                    type: LOAD_DETAILS,
+                    payload: {
+                        id
+                    }
+                })
+            }
+        }
     }
 }
 
 export default function* peopleSaga() {
-    yield fork(fetchPeopleOnRoute);
-    yield takeLatest(LOAD_PEOPLE, fetchPeopleList);
+    yield fork(fetchOnRoute);
+    yield takeLatest(LOAD_PEOPLE, fetchPeople);
+    yield takeLatest(LOAD_DETAILS, fetchDetails);
 }
